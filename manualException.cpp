b01ed7e636ec1a1e66453312dba363209ca55445
marchdillo <kevinno.limiy@gmail.com>
2025-06-09 19:27:17 +0700

    menambahkan int main

--- manualException.cpp
+++ manualException.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a)
+    {
+        cout << "Pengecualin akan dieksekusi" << endl;
+    }
+    catch (...)
+    {
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    return 0;
+}
