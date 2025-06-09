ad226deef592606d4480ebaaf5ab12c63b0f649c
marchdillo <kevinno.limiy@gmail.com>
2025-06-09 18:54:50 +0700

    menambahkan int main string, cout dan cin

--- ioFile.cpp
+++ ioFile.cpp
@@ -2,3 +2,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    string baris;
+    string namaFile;
+    cout << "Masukkan nama file: ";
+    cin >> namaFile;
+};
